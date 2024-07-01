a3cd20db315cb2b592e6035d991c0c59e497b1b3
Twans <82774312+Twans99@users.noreply.github.com>
2024-07-02 04:09:57 +0700

    Delete Car_info/src/pages directory

--- Car_info/src/pages/cars.jsx
+++
@@ -1,9 +0,0 @@
-function cars() {
-    return (
-        <div className="">
-            <p className="">Cars</p>
-        </div>
-    )
-}
-
-export default cars
--- Car_info/src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-function Home(){
-    return(
-        <div className="">
-            <p className=""> Home page</p>
-        </div>
-    )
-}
-
-export default Home
